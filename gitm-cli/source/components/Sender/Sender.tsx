898eb543441e081b6fe15b3a99bf48d84d3f7bfb
rifatNR <Rifatnoor92@gmail.com>
2024-02-27 12:46:27 +0600

    changes

--- gitm-cli/source/components/Sender/Sender.tsx
+++ gitm-cli/source/components/Sender/Sender.tsx
@@ -4,12 +4,15 @@ import SenderList from '../Misc/SenderList.js';
 import {Spinner} from '../Misc/Spinner.js';
 
 const Sender = () => {
+	const path = '/home/rifat/Works/gitm/gitm-cli/send_files' || process.cwd(); // FIXME: FIX Static
+	const fileName = process.argv[3];
+	const filePath = `${path}/${fileName}`;
+
 	return (
 		<Box flexDirection="column">
 			<Text>
 				<Spinner /> Sending
 			</Text>
-			{/* <SenderList /> */}
 		</Box>
 	);
 };
