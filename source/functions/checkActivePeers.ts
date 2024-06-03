9e6747370bda31f3a78ae9c31a003ba02f8f0545
rifatNR <Rifatnoor92@gmail.com>
2024-06-03 18:23:36 +0600

    fix bug

--- source/functions/checkActivePeers.ts
+++ source/functions/checkActivePeers.ts
@@ -1,21 +1,29 @@
-import {useEffect} from 'react';
+import {useCallback, useEffect} from 'react';
 import {$peers} from '../stores/baseStore.js';
 import {useStore} from '@nanostores/react';
 
 export const useActivePeers = () => {
 	const peers = useStore($peers);
 
-	useEffect(() => {
-		peers.forEach(peer => {
-			console.log('🚧');
-			fetch(`http:/${peer.ip}:${peer.httpPort}/get-active-status`)
+	const pollingPeers = useCallback(
+		(ip: string, port: number) => {
+			fetch(`http:/${ip}:${port}/get-active-status`)
 				.then(response => response.json())
 				.then(data => {
 					console.log(data);
+					pollingPeers(ip, port);
 				})
 				.catch(error => {
 					console.error('Error:', error);
 				});
+		},
+		[peers],
+	);
+
+	useEffect(() => {
+		peers.forEach(peer => {
+			console.log('🚧');
+			pollingPeers(peer.ip, peer.httpPort);
 		});
 	}, [peers]);
 };
