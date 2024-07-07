1599885326353ee267d0ee6b35f64ee426f37725
rifatNR <Rifatnoor92@gmail.com>
2024-07-08 03:26:22 +0600

    refactor

--- source/functions/commands.ts
+++ source/functions/commands.ts
@@ -6,39 +6,7 @@ import {useEffect, useMemo} from 'react';
 import {$action, $sendingFiles} from '../stores/baseStore.js';
 
 export const useCommands = () => {
-	// const commands = useMemo(() => process.argv?.slice(2), []);
-
-	// useEffect(() => {
-	// 	const program = new Command();
-
-	// 	program
-	// 		.name('bomba')
-	// 		.description('A CLI tool to do cool stuffs.')
-	// 		.version('1.0.0')
-	// 		.option('-n, --name <char>', "Change peer's name")
-	// 		.arguments('[files...]')
-	// 		.action((files, options) => {
-	// 			if (options.name) {
-	// 				console.log(`Hey, ${options.name}\n`);
-	// 			}
-
-	// 			if (files && files.length > 0) {
-	// 				console.log('Files:', files, '\n');
-	// 				$action.set('SEND');
-	// 				$sendingFiles.set(files);
-	// 			} else {
-	// 				console.log('Waiting to receive.\n');
-	// 				$action.set('RECEIVE');
-	// 			}
-	// 		});
-
-	// 	program.parse();
-
-	// 	return () => {};
-	// }, []);
-
 	useEffect(() => {
-		// ! TEST: pnpm start .\Brain.png .\brainfuck_code.png
 		yargs(hideBin(process.argv))
 			.command(
 				'chat',
@@ -84,3 +52,4 @@ export const useCommands = () => {
 // ! bomba
 // ! bomba --version
 // ! bomba --name Mr. Ghost | bomba --name "Mr. Ghost"
+// ! bomba .\Brain.png .\brainfuck_code.png
