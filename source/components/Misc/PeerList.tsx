447220a98ca7986bbf74929b2835389a6fada99f
rifatNR <Rifatnoor92@gmail.com>
2024-07-17 17:22:05 +0600

    added new Peers list component

--- source/components/Misc/PeerList.tsx
+++ source/components/Misc/PeerList.tsx
@@ -1,103 +1,71 @@
-import React, {useEffect, useMemo, useState} from 'react';
-import {Box, Newline, Text, useInput, useStdin} from 'ink';
+import React, {useMemo} from 'react';
+// import {render} from 'ink';
+import SelectInput from 'ink-select-input';
 import {ConnectedPeersType} from '../../stores/peersStore.js';
-import {useFileDownloader} from '../../functions/useFileDownloader.js';
+import {Box} from 'ink';
 import {useStore} from '@nanostores/react';
 import {$transferInfo} from '../../stores/fileHandlerStore.js';
-import FileTransfer from './FileTransfer.js';
-import ProgressBar from './ProgressBar.js';
-import {logToFile} from '../../functions/log.js';
-import {$baseInfo} from '../../stores/baseStore.js';
+import {useFileDownloader} from '../../functions/useFileDownloader.js';
 import {useHashCheck} from '../../functions/useHashCheck.js';
 
+type ItemType = {
+	label: string;
+	value: string;
+};
+type ItemsType = ItemType[];
 type PropsType = {
 	peers: ConnectedPeersType;
 };
-
-export default function PeerList({peers}: PropsType) {
-	if (!peers) throw new Error('No sender found');
-
+const PeerList = ({peers}: PropsType) => {
 	const transferInfo = useStore($transferInfo);
 
-	const peersIds = useMemo(() => Object.keys(peers).map(key => key), [peers]);
-	const [selectedIndex, setSelectedIndex] = useState<number>(0);
+	const items: ItemsType = useMemo(
+		() =>
+			Object.entries(peers)?.map(([key, value]) => ({
+				label: value.name,
+				value: key,
+			})),
+		[peers],
+	);
 
-	useInput((input, key) => {
-		if (key.downArrow) {
-			setSelectedIndex(prevIndex => (prevIndex + 1) % peersIds.length);
-		} else if (key.upArrow) {
-			setSelectedIndex(
-				prevIndex => (prevIndex - 1 + peersIds.length) % peersIds.length,
-			);
-		} else if (key.return) {
-			const peerID = peersIds[selectedIndex];
-			if (!peerID) {
-				console.log('Peer ID not found');
-				return;
-			}
-			const selectedPeer = peers[peerID];
-			const selectedPeerTransferInfo = transferInfo[peerID]?.files;
+	const handleSelect = (item: ItemType) => {
+		const selectedPeer = peers[item.value];
+		const selectedPeerTransferInfo = transferInfo[item.value];
 
-			if (!selectedPeer) {
-				console.log('Selected Peer not found');
-				return;
-			}
-			if (!selectedPeerTransferInfo) {
-				console.log('⭕ No sending files found ⭕');
-				return;
-			}
-
-			// console.log(selectedPeer, fileNames);
-
-			Object.entries(selectedPeerTransferInfo)?.forEach(
-				async ([key, value]) => {
-					// console.log(`Downloading: ${value.fileName}`);
-					await useFileDownloader(
-						selectedPeer.id,
-						selectedPeer.ip,
-						selectedPeer.httpPort,
-						key,
-						value.fileName,
-					);
-					await useHashCheck(
-						selectedPeer.id,
-						selectedPeer.ip,
-						selectedPeer.httpPort,
-						key,
-						value.fileName,
-					);
-				},
-			);
+		if (!selectedPeer) {
+			console.log('Selected Peer not found');
+			return;
 		}
-	});
+		if (!selectedPeerTransferInfo) {
+			console.log('⭕ No sending files found ⭕');
+			return;
+		}
 
-	useEffect(() => {
-		logToFile('transferInfo', $baseInfo.get().MY_NAME, transferInfo);
-	}, [transferInfo]);
+		const files = selectedPeerTransferInfo.files;
+		Object.entries(files)?.forEach(async ([key, value]) => {
+			console.log(`Downloading: ${value.fileName}`);
+			await useFileDownloader(
+				selectedPeer.id,
+				selectedPeer.ip,
+				selectedPeer.httpPort,
+				key,
+				value.fileName,
+			);
+			await useHashCheck(
+				selectedPeer.id,
+				selectedPeer.ip,
+				selectedPeer.httpPort,
+				key,
+				value.fileName,
+			);
+		});
+	};
 
 	return (
-		<Box flexDirection="column" marginTop={1} marginLeft={1}>
-			{Object.keys(peers).map(key => (
-				<Box key={key} flexDirection="column">
-					<Box
-						borderColor={key === peersIds[selectedIndex] ? 'green' : 'black'}
-						borderStyle={key === peersIds[selectedIndex] ? 'bold' : 'single'}
-						paddingX={1}
-					>
-						<Box flexDirection="column">
-							<Box>
-								<Text backgroundColor="green" color="white" bold>
-									{' '}
-									{peers[key]?.name}{' '}
-								</Text>
-							</Box>
-							{transferInfo[key] && (
-								<FileTransfer peerID={key} transferData={transferInfo[key]!} />
-							)}
-						</Box>
-					</Box>
-				</Box>
-			))}
+		<Box marginTop={1} marginLeft={1}>
+			<SelectInput items={items} onSelect={handleSelect} />
 		</Box>
 	);
-}
+};
+
+export default PeerList;
