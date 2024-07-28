a0030db03d617ec4d1ed2dab06b7bcaad5b8dbc5
rifatNR <Rifatnoor92@gmail.com>
2024-07-28 09:14:23 +0600

    possible fix

--- source/functions/useFileDownloader.ts
+++ source/functions/useFileDownloader.ts
@@ -34,7 +34,8 @@ import {CustomError} from './error.js';
 // 		});
 // };
 
-export const useFileDownloader = (
+// TODO:: DELETE THIS FUNCTION
+export const OLD_useFileDownloader = (
 	PEER_ID: string,
 	PEER_IP: string,
 	PEER_TCP_PORT: number,
@@ -121,5 +122,91 @@ export const useFileDownloader = (
 				updateTransferFileError(FILE_ID, err.data.detail);
 				reject(err);
 			});
+	});
+};
+
+export const useFileDownloader = (
+	PEER_ID: string,
+	PEER_IP: string,
+	PEER_TCP_PORT: number,
+	FILE_ID: string,
+	FILENAME: string,
+): Promise<void> => {
+	const url = `http://${PEER_IP}:${PEER_TCP_PORT}/download/${FILENAME}`;
+	const outputPath = `${process.cwd()}/receive_files/${FILENAME}`; // TODO:: Fix
+
+	return new Promise<void>(async (resolve, reject) => {
+		try {
+			const res = await fetch(url);
+			if (!res.ok) {
+				const errorMessage =
+					res.status == 404 ? 'File not found!' : 'Failed to download file!';
+				throw new CustomError('Failed to download file', {
+					code: res.status,
+					detail: errorMessage,
+				});
+			}
+
+			const totalLength = parseInt(
+				res.headers.get('content-length') || '0',
+				10,
+			);
+			const writer = fs.createWriteStream(outputPath);
+			let downloaded = 0;
+			let progress = 0;
+
+			const reader = res.body!.getReader();
+
+			const pump = async () => {
+				try {
+					const {done, value} = await reader.read();
+					if (done) {
+						writer.end();
+						resolve();
+						return;
+					}
+					writer.write(value);
+					downloaded += value.length;
+					progress = parseFloat(((downloaded / totalLength) * 100).toFixed(2));
+
+					updateTransferProgress(FILE_ID, {
+						state: progress < 100 ? 'TRANSFERRING' : 'TRANSFERRED',
+						progress: progress,
+						fileName: FILENAME,
+						totalSize: totalLength,
+						downloadedSize: downloaded,
+					});
+
+					await pump();
+				} catch (error) {
+					writer.end();
+					updateTransferFileState(FILE_ID, 'ERROR');
+					updateTransferFileError(FILE_ID, '--------');
+					reject(
+						new CustomError('Failed to read file', {
+							code: 410,
+							detail: 'Failed to read file!',
+						}),
+					);
+				}
+			};
+
+			writer.on('error', () => {
+				reject(
+					new CustomError('Failed to write file', {
+						code: 410,
+						detail: 'Failed to write file!',
+					}),
+				);
+			});
+
+			await pump();
+		} catch (err) {
+			if (err instanceof CustomError) {
+				updateTransferFileState(FILE_ID, 'ERROR');
+				updateTransferFileError(FILE_ID, err.data.detail);
+			}
+			reject(err);
+		}
 	});
 };
