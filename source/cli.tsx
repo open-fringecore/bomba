37c3556e54af759b3216d4005a118809c50fc439
rifatNR <Rifatnoor92@gmail.com>
2024-09-13 09:18:22 +0600

    added help prompt

--- source/cli.tsx
+++ source/cli.tsx
@@ -7,16 +7,25 @@ import tty from 'node:tty';
 
 const cli = meow(
 	`
-	Usage
-	  $ khamba
+	khamba - A tool for sending and receiving files via local network.
 
-	Options
-		--name  Your name
-		-v, --version  Show version number
+	Usage:
+		TO SEND:
+			$ khamba file1.jpg folder01/file2.mp4 ../folder02/file3.mp4
 
-	Examples
-	  $ khamba --name=Jane
-	  Hello, Jane
+		TO RECEIVE:
+			$ khamba
+
+	Options:
+		-n, --name      Your preferred name
+		-v, --version   Show version number
+
+	Examples:
+		$ khamba --name=Jane
+		⢎⡑ Receiving...
+
+		$ khamba -n I_AM_GROOT file1.jpg folder01/file2.mp4 ../folder02/file3.mp4
+		⢎⡑ Sending...
 `,
 	{
 		importMeta: import.meta,
